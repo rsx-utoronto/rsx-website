0ced9fb6a87aa08829d5e7722f0169736c3e0b21
Alexey Albert <61938362+alexeyalbert@users.noreply.github.com>
2025-09-10 03:28:59 -0400

    move +layout.ts to +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,22 @@
 	import Navbar from '$lib/components/Navbar.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 
+	import posthog from 'posthog-js';
+	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
+
+	export const load = async () => {
+		if (browser) {
+			posthog.init('phc_idu7SeQKlqVqddStc6dxdH8rfCTmAr1kDRN7NvfPeqp', {
+				api_host: 'https://us.i.posthog.com',
+				defaults: '2025-05-24',
+				person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
+			});
+		}
+
+		return;
+	};
+
 	let { children } = $props();
 </script>
 
